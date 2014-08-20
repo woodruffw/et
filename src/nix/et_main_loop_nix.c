4100e9d3d5d2fbf14501cc35c148d665edcc9ee5
William Woodruff <william@tuffbizz.com>
2014-08-20 11:23:02 -0400

    removed unneeded variables

--- src/nix/et_main_loop_nix.c
+++ src/nix/et_main_loop_nix.c
@@ -17,11 +17,13 @@
 */
 void et_main_loop_nix(int socket, char *nick)
 {
+	char message[IRC_MSGLEN];
 	char buf[1024];
 	bool auth = false;
 
 	while(1)
 	{
+		memset(message, 0, IRC_MSGLEN);
 		memset(buf, 0, 1024);
 		recv(socket, buf, 1023, 0);
 		buf[1024] = '\0';
@@ -42,27 +44,25 @@ void et_main_loop_nix(int socket, char *nick)
 			if (strstr(cmd, "auth") && !(strstr(cmd, "deauth")))
 			{
 				cmd += 5; /* increment past 'auth' */
-				char auth_message[IRC_MSGLEN];
 
 				if (!strcmp(cmd, IRC_AUTH))
 				{
 					auth = true;
-					snprintf(auth_message, IRC_MSGLEN, "PRIVMSG %s :Successfully authorized. %s listening.\r\n", IRC_CHANNEL, nick);
-					send(socket, auth_message, strlen(auth_message), 0);
+					snprintf(message, IRC_MSGLEN, "PRIVMSG %s :Successfully authorized. %s listening.\r\n", IRC_CHANNEL, nick);
+					send(socket, message, strlen(message), 0);
 				}
 				else
 				{
-					snprintf(auth_message, IRC_MSGLEN, "PRIVMSG %s :Authorization failed, password incorrect.\r\n", IRC_CHANNEL);
-					send(socket, auth_message, strlen(auth_message), 0);
+					snprintf(message, IRC_MSGLEN, "PRIVMSG %s :Authorization failed, password incorrect.\r\n", IRC_CHANNEL);
+					send(socket, message, strlen(message), 0);
 				}
 			}
 			else
 			{
 				if (!auth)
 				{
-					char unauth_message[IRC_MSGLEN];
-					snprintf(unauth_message, IRC_MSGLEN, "PRIVMSG %s :Not authorized to command. Please auth.\r\n", IRC_CHANNEL);
-					send(socket, unauth_message, strlen(unauth_message), 0);
+					snprintf(message, IRC_MSGLEN, "PRIVMSG %s :Not authorized to command. Please auth.\r\n", IRC_CHANNEL);
+					send(socket, message, strlen(message), 0);
 				}
 				else
 				{
@@ -72,9 +72,8 @@ void et_main_loop_nix(int socket, char *nick)
 					}
 					else if (!strcmp(cmd, "deauth"))
 					{
-						char deauth_message[IRC_MSGLEN];
-						snprintf(deauth_message, IRC_MSGLEN, "PRIVMSG %s :%s deauthorized. Reauth to control.\r\n", IRC_CHANNEL, nick);
-						send(socket, deauth_message, strlen(deauth_message), 0);
+						snprintf(message, IRC_MSGLEN, "PRIVMSG %s :%s deauthorized. Reauth to control.\r\n", IRC_CHANNEL, nick);
+						send(socket, message, strlen(message), 0);
 						auth = false;
 					}
 					else if (!strcmp(cmd, "info"))
@@ -82,51 +81,44 @@ void et_main_loop_nix(int socket, char *nick)
 						struct utsname sys_info;
 						uname(&sys_info);
 
-						char info[IRC_MSGLEN];
+						snprintf(message, IRC_MSGLEN, "PRIVMSG %s :Kernel: %s\r\n", IRC_CHANNEL, sys_info.sysname);
+						send(socket, message, strlen(message), 0);
+						memset(message, 0, IRC_MSGLEN);
 
-						snprintf(info, IRC_MSGLEN, "PRIVMSG %s :Kernel: %s\r\n", IRC_CHANNEL, sys_info.sysname);
-						send(socket, info, strlen(info), 0);
-						memset(info, 0, IRC_MSGLEN);
+						snprintf(message, IRC_MSGLEN, "PRIVMSG %s :Host: %s\r\n", IRC_CHANNEL, sys_info.nodename);
+						send(socket, message, strlen(message), 0);
+						memset(message, 0, IRC_MSGLEN);
 
-						snprintf(info, IRC_MSGLEN, "PRIVMSG %s :Host: %s\r\n", IRC_CHANNEL, sys_info.nodename);
-						send(socket, info, strlen(info), 0);
-						memset(info, 0, IRC_MSGLEN);
+						snprintf(message, IRC_MSGLEN, "PRIVMSG %s :Release: %s\r\n", IRC_CHANNEL, sys_info.release);
+						send(socket, message, strlen(message), 0);
+						memset(message, 0, IRC_MSGLEN);
 
-						snprintf(info, IRC_MSGLEN, "PRIVMSG %s :Release: %s\r\n", IRC_CHANNEL, sys_info.release);
-						send(socket, info, strlen(info), 0);
-						memset(info, 0, IRC_MSGLEN);
+						snprintf(message, IRC_MSGLEN, "PRIVMSG %s :Version: %s\r\n", IRC_CHANNEL, sys_info.version);
+						send(socket, message, strlen(message), 0);
+						memset(message, 0, IRC_MSGLEN);
 
-						snprintf(info, IRC_MSGLEN, "PRIVMSG %s :Version: %s\r\n", IRC_CHANNEL, sys_info.version);
-						send(socket, info, strlen(info), 0);
-						memset(info, 0, IRC_MSGLEN);
-
-						snprintf(info, IRC_MSGLEN, "PRIVMSG %s :Arch: %s\r\n", IRC_CHANNEL, sys_info.machine);
-						send(socket, info, strlen(info), 0);
-						memset(info, 0, IRC_MSGLEN);
+						snprintf(message, IRC_MSGLEN, "PRIVMSG %s :Arch: %s\r\n", IRC_CHANNEL, sys_info.machine);
+						send(socket, message, strlen(message), 0);
+						memset(message, 0, IRC_MSGLEN);
 					}
 					else if (strstr(cmd, "popup"))
 					{
-						char nopopup[IRC_MSGLEN];
-						snprintf(nopopup, IRC_MSGLEN, "PRIVMSG %s :%s is nix and cannot create popups.\r\n", IRC_CHANNEL, nick);
-						send(socket, nopopup, strlen(nopopup), 0);
+						snprintf(message, IRC_MSGLEN, "PRIVMSG %s :%s is nix and cannot create popups.\r\n", IRC_CHANNEL, nick);
+						send(socket, message, strlen(message), 0);
 					}
 					else if (!strcmp(cmd, "persist"))
 					{
-						char nopersist[IRC_MSGLEN];
-						snprintf(nopersist, IRC_MSGLEN, "PRIVMSG %s :%s is nix and cannot persist.\r\n", IRC_CHANNEL, nick);
-						send(socket, nopersist, strlen(nopersist), 0);
+						snprintf(message, IRC_MSGLEN, "PRIVMSG %s :%s is nix and cannot persist.\r\n", IRC_CHANNEL, nick);
+						send(socket, message, strlen(message), 0);
 					}
 					else if (!strcmp(cmd, "depersist"))
 					{
-						char nodepersist[IRC_MSGLEN];
-						snprintf(nodepersist, IRC_MSGLEN, "PRIVMSG %s :%s is nix and cannot depersist.\r\n", IRC_CHANNEL, nick);
-						send(socket, nodepersist, strlen(nodepersist), 0);
+						snprintf(message, IRC_MSGLEN, "PRIVMSG %s :%s is nix and cannot depersist.\r\n", IRC_CHANNEL, nick);
+						send(socket, message, strlen(message), 0);
 					}
 					else
 					{
 						char cmd_output[IRC_MSGLEN];
-						char cmd_message[IRC_MSGLEN];
-
 						FILE *output_file = popen(cmd, "r");
 						fread((void *) cmd_output, 1, IRC_MSGLEN, output_file);
 
@@ -141,14 +133,14 @@ void et_main_loop_nix(int socket, char *nick)
 
 						if (strlen(cmd_output) >= 500)
 						{
-							snprintf(cmd_message, IRC_MSGLEN, "PRIVMSG %s :Command output too large to send.\r\n", IRC_CHANNEL);
+							snprintf(message, IRC_MSGLEN, "PRIVMSG %s :Command output too large to send.\r\n", IRC_CHANNEL);
 						}
 						else
 						{
-							snprintf(cmd_message, IRC_MSGLEN, "PRIVMSG %s :%s\r\n", IRC_CHANNEL, cmd_output);
+							snprintf(message, IRC_MSGLEN, "PRIVMSG %s :%s\r\n", IRC_CHANNEL, cmd_output);
 						}
 
-						send(socket, cmd_message, strlen(cmd_message), 0);
+						send(socket, message, strlen(message), 0);
 						pclose(output_file);
 						memset(cmd_output, 0, IRC_MSGLEN);
 					}
